84151162d8439b1bc359efa04d69578afee0e77c
John Gemignani <jrgemignani@gmail.com>
2018-08-06 00:13:19 -0700

    Making TreeNode more generic -jrg

--- TreeNode.java
+++ TreeNode.java
@@ -1,7 +1,8 @@
-// TreeNode class for LeetCode problems.
-// It contains methods for building TreeNode trees from 
-// Binary Tree arrays print the node values. 
-// It is expected that the nodes of the tree have unique values.
+// Generic TreeNode class for help with LeetCode problems. But, not 
+// completely compatible with them.
+// It contains methods for building TreeNode trees from Binary Tree 
+// arrays print the node values. It is expected that the nodes of 
+// the tree have unique values.
 //
 
 import java.util.Map;
@@ -11,24 +12,41 @@ public class TreeNode {
 
    private TreeNode left = null;
    private TreeNode right = null;
-   private final int val;
+   private final int value;
 
    public TreeNode(int value){
-      this.val = value;
+      this.value = value;
+   }
+
+   public TreeNode getLeftChild(){
+      return this.left;
+   }
+
+   public TreeNode getRightChild(){
+      return this.right;
+   }
+
+   public int getValue(){
+      return this.value;
+   }
+
+   @Override
+   public String toString(){
+      return "[" + this.value + "]";
    }
 
    // method to create a tree from a binary tree array
-   private static TreeNode createTree(Integer[] btreeArray){
+   public static TreeNode createTree(Integer[] btreeArray){
       // return null if null BT array is passed
       if (btreeArray == null) return null;
 
       // create a map of node value to TreeNode 
       Map<Integer, TreeNode> map = new HashMap<Integer, TreeNode>();
       // iterate through the BT array and create the TreeNodes
-      for (Integer val : btreeArray){
-         if (val != null){
-            TreeNode node = new TreeNode(val);
-            map.put(val, node);
+      for (Integer value : btreeArray){
+         if (value != null){
+            TreeNode node = new TreeNode(value);
+            map.put(value, node);
          }
       }
       // now iterate and add the links to the children
@@ -49,24 +67,16 @@ public class TreeNode {
       return map.get(btreeArray[0]);
    }
 
-   // method to find a node, by it's value, in the tree
-   private static TreeNode findNode(TreeNode root, int val){
+   // method to find a TreeNode, by it's value, in the tree
+   public static TreeNode findNode(TreeNode root, int value){
       // if the node is null, return null
       if (root == null) return null;
       // if the node value equals the passed value, return the node
-      if (root.val == val) return root;
+      if (root.value == value) return root;
       // check left side
-      TreeNode temp = findNode(root.left, val);
+      TreeNode temp = findNode(root.left, value);
       if (temp != null) return temp;
       // check right side
-      return findNode(root.right, val);
-   }
-      
-   // helper method to print a node
-   private static void printNode(TreeNode node){
-      System.out.print("[");
-      if (node != null)
-         System.out.print(node.val);
-      System.out.println("]");
+      return findNode(root.right, value);
    }
 }
